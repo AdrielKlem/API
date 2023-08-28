58fae2b1b0f7b9766454341a1f64b92543d47602
Adriel Klem <adrielklem16@gmail.com>
2023-08-28 17:53:12 -0300

    update server.js add response data

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (request, response) => {
+    response.send("Hello, World!")
+});
+
 const PORT = 3333; // Door to localize 
 
 app.listen(PORT/* Waiter */, () => console.log(`Server is running on Port ${PORT}`));
