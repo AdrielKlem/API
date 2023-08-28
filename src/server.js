d9b16afe73e28ae6ca4370c2de6027e68ae4f399
Adriel Klem <adrielklem16@gmail.com>
2023-08-28 18:42:30 -0300

    update server.js add router query

--- src/server.js
+++ src/server.js
@@ -8,7 +8,16 @@ app.get("/message/:id/:user", (request, response) => {
     response.send(`
     Mensagem ID: ${id}. 
     Para o usuário: ${user}.`
-    )
+    );
+});
+
+app.get("/users", (request, response) => {
+    const { page, limit} = request.query
+
+    response.send(`
+    Página: ${page}. 
+    Mostrar: ${limit}.`
+    );
 });
 
 const PORT = 3333; // Door to localize 
